7c4c52beb42ea68a5e42b95351902d54a8f74bfc
IanOlmedo <i.olmedo@alumno.um.edu.ar>
2024-11-11 15:56:03 -0300

    first commit

---
+++ src/main/java/org/example/Figuras.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Figuras {
+    Vector<Cuadrado> cuadrados=new Vector<Cuadrado>();
+    Vector<Circulo> circulos= new Vector<Circulo>();
+    public void addCirculo(Circulo c){
+        circulos.add(c);
+    }
+    public void addCuadrado(Cuadrado c){
+        cuadrados.add(c);
+    }
+    public void dibujarFiguras(){
+        Enumeration<Cuadrado> cuads=cuadrados.elements();
+        Cuadrado c;
+        while (cuads.hasMoreElements()){
+            c=cuads.nextElement();
+            c.dibujar();
+        }
+        Enumeration<Circulo> circs=circulos.elements();
+        Circulo ci;
+        while (cuads.hasMoreElements()){
+            ci=circs.nextElement();
+            ci.dibujar();
+        }
+    }
+}
